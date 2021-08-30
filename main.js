5d07e9b2a48b5174735629911ab623065e4f8048
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2021-08-30 19:35:11 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -9,4 +9,19 @@ recognition.onresult=function run(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textBox").innerHTML=content;
+    speak();
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=document.getElementById("textBox").value;
+    var utterThis=new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+ Webcam.attach(camera);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("#camera");
