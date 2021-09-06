3393f4872865819f0b177eb6809daaff8da62f6f
ashin1234 <78942006+ashin1234@users.noreply.github.com>
2021-09-06 19:28:42 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-var SpeechRecognition=window.webkitSpeechRecognition;
-var recognition=new SpeechRecognition();
-function start(){
-    document.getElementById("textBox").innerHTML="";
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start() {
+    document.getElementById("textBox").innerHTML = "";
     recognition.start();
 }
-recognition.onresult=function run(event){
+recognition.onresult = function run(event) {
     console.log(event);
-    var content=event.results[0][0].transcript;
+    var content = event.results[0][0].transcript;
     console.log(content);
-    document.getElementById("textBox").innerHTML=content;
-    speak();
+    document.getElementById("textBox").innerHTML = content;
+    if (content == "take my selfie") {
+        speak();
+    }
 }
-function speak(){
-    var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textBox").value;
-    var utterThis=new SpeechSynthesisUtterance(speak_data);
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data = "taking your selfie in 5 secondes";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
- Webcam.attach(camera);
+    Webcam.attach(camera);
+    setTimeout(function () {
+        take_snapshot();
+        save();
+    }, 5000);
+}
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = "<img id='selfie_image' src='" + data_uri + "'/>";
+    });
 }
 Webcam.set({
-    width:360,
-    height:250,
-    image_format:'png',
-    png_quality:90
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
 });
-camera=document.getElementById("camera");
+camera = document.getElementById("camera");
+function save() {
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
